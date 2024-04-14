4ceb152a5d9c27a5687c9700e45721a14c57ab82
Franci <f.dhima29@gmail.com>
2024-04-14 18:05:49 +0200

    Button Click exercise

---
+++ Desktop/MERN BetaPlan 2024/WebFun/Javascript/ButtonClick/script.js
@@ -0,0 +1,14 @@
+function message() { 
+    alert("Ninja was clicked");
+
+
+ }
+
+ function change(element){
+    element.innerText="LogOut";
+
+ }
+
+ function remove(element){
+    element.remove();
+ }
